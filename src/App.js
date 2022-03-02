96c516cc0a57d136a70b5745b8bb87279f9d600c
Sashankr <sashankr97@gmail.com>
2022-03-02 23:46:26 +0530

    fetching the data with fetch api

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import './App.css';
 
+import { useEffect } from 'react';
+
 function App() {
+
+  useEffect(()=>{
+    fetch('https://api.spaceflightnewsapi.net/v3/articles')
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+  },[])
+
   return (
     <div className="App">
       <div className='title'>
         <h1>Space News</h1>
       </div>
       <div className='news-container'>
-        
+
       </div>
     </div>
   );
